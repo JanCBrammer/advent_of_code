f0258f9e440900fadca5c44640b78f5d3fedc592
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-12-05 18:28:34 +0000

    Add day 1

---
+++ day01/tests.py
@@ -0,0 +1,28 @@
+import pytest
+import solution
+
+
+@pytest.mark.parametrize(
+    "directions, floor",
+    [
+        ("(())", 0),
+        ("()()", 0),
+        ("(((", 3),
+        ("(()(()(", 3),
+        ("))(((((", 3),
+        ("())", -1),
+        ("))(", -1),
+        (")))", -3),
+        (")())())", -3),
+    ],
+)
+def test_find_floor(directions: str, floor: int):
+    assert solution.find_floor(directions) == floor
+
+
+@pytest.mark.parametrize(
+    "directions, position",
+    [(")", 1), ("()())", 5)],
+)
+def test_find_basement_entrance_direction(directions: str, position: int):
+    assert solution.find_basement_entrance_direction(directions) == position
