426358a6e1a07f731390203dc74e51ab3a504281
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-12-07 18:57:20 +0000

    Add day00 template

---
+++ day00/solution.py
@@ -0,0 +1,27 @@
+"""
+
+--- Notes ---
+
+"""
+from pathlib import Path
+
+
+def parse_input(input_path: str) -> str:
+
+    return Path(input_path).read_text()
+
+
+def solve_part1(input_path: str):
+
+    print(f"Part 1:\n...\n")
+
+
+def solve_part2(input_path: str):
+
+    print(f"Part 2:\n...\n")
+
+
+if __name__ == "__main__":
+
+    solve_part1("input.txt")
+    solve_part2("input.txt")
---
+++ day00/tests.py
@@ -0,0 +1,13 @@
+import pytest
+import solution
+
+
+@pytest.mark.parametrize(
+    "foo, bar",
+    [
+        ("baz", 42),
+    ],
+)
+def test_find_bar(foo: str, bar: int):
+
+    assert solution.find_bar(foo) == bar
