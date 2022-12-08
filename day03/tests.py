cf0d8b866a24fcb3175740b70f9e667fadbf1180
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-12-08 07:12:46 +0000

    Add day 3

---
+++ day03/tests.py
@@ -0,0 +1,45 @@
+import pytest
+import solution
+
+
+@pytest.mark.parametrize(
+    "directions, visited_houses",
+    [
+        (">", {(0, 0), (1, 0)}),
+        ("^>v<", {(0, 0), (0, 1), (1, 1), (1, 0)}),
+        ("^v^v^v^v^v", {(0, 0), (0, 1)}),
+    ],
+)
+def test_track_visited_houses(directions: str, visited_houses: set[tuple[int, int]]):
+
+    assert solution.track_visited_houses(directions) == visited_houses
+
+
+@pytest.mark.parametrize(
+    "directions, visited_houses",
+    [
+        ("^v", {(0, -1), (0, 0), (0, 1)}),
+        ("^>v<", {(0, -1), (0, 0), (0, 1)}),
+        (
+            "^v^v^v^v^v",
+            {
+                (0, 1),
+                (0, -3),
+                (0, 4),
+                (0, 0),
+                (0, 3),
+                (0, -2),
+                (0, -4),
+                (0, 2),
+                (0, 5),
+                (0, -5),
+                (0, -1),
+            },
+        ),
+    ],
+)
+def test_track_visited_houses_robotsanta(
+    directions: str, visited_houses: set[tuple[int, int]]
+):
+
+    assert solution.track_visited_houses_robotsanta(directions) == visited_houses
